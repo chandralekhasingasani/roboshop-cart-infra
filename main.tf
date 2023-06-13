e0d0e45f9bb2951e21b9b2de975822b5e2930368
chandralekhasingasani <chandralekha.s@nutanix.com>
2023-06-13 11:59:30 +0530

    test

--- main.tf
+++ main.tf
@@ -5,7 +5,12 @@ module "vpc"{
   ENV                 = var.ENV
   SUBNET_CIDR_BLOCK   = var.SUBNET_CIDR_BLOCK
   COMPONENT           = var.COMPONENT
-  
+  DEFAULT_VPC_ID      = data.terraform_remote_state.tgw.outputs.DEFAULT_VPC_ID
+  DEFAULT_VPC_CIDR_BLOCK = data.terraform_remote_state.tgw.outputs.DEFAULT_VPC_CIDR_BLOCK
+  TGW_ID              = data.terraform_remote_state.tgw.outputs.TGW_ID
+  TGW_DEFAULT_ATTACHMENT_ID = data.terraform_remote_state.tgw.outputs.TGW_DEFAULT_ATTACHMENT_ID
+  TGW_DEFAULT_ROUTE_TABLE_ID = data.terraform_remote_state.tgw.outputs.TGW_DEFAULT_ROUTE_TABLE_ID
+  PUBLIC_ROUTE_TABLE_ID_DEFAULT_VPC = data.terraform_remote_state.tgw.outputs.PUBLIC_ROUTE_TABLE_ID_DEFAULT_VPC
 }
 
 output "DEFAULT_VPC_CIDR_BLOCK" {
