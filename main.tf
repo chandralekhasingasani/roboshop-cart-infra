934ced9d0b2c5c118a764f92720880353b85cb66
chandralekhasingasani <chandralekha.s@nutanix.com>
2023-06-13 10:19:56 +0530

    test

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+/*
 module "vpc"{
   source              ="git::https://github.com/chandralekhasingasani/tf-module-vpc.git"
   CIDR_BLOCK          = var.CIDR_BLOCK
@@ -11,4 +12,8 @@ module "vpc"{
   TGW_DEFAULT_ATTACHMENT_ID = data.terraform_remote_state.tgw.outputs.TGW_DEFAULT_ATTACHMENT_ID
   TGW_DEFAULT_ROUTE_TABLE_ID = data.terraform_remote_state.tgw.outputs.TGW_DEFAULT_ROUTE_TABLE_ID
   PUBLIC_ROUTE_TABLE_ID_DEFAULT_VPC = data.terraform_remote_state.tgw.outputs.PUBLIC_ROUTE_TABLE_ID_DEFAULT_VPC
+}*/
+
+output "DEFAULT_VPC_CIDR_BLOCK" {
+  value = data.terraform_remote_state.tgw.outputs.DEFAULT_VPC_CIDR_BLOCK
 }
